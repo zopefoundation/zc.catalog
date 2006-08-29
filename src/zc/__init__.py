deaf7b9744bb228724bd920747e3c3900fd5ab51
Fred Drake <fdrake@acm.org>
2006-08-29 15:36:38 +0000

    - degrade more reasonably if pkg_resources is not available - remove silliness when importing pkg_resources

--- src/zc/__init__.py
+++ src/zc/__init__.py
@@ -1,5 +1,7 @@
 # this is a namespace package
 try:
-    __import__('pkg_resources').declare_namespace(__name__)
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
 except ImportError:
-    pass
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
