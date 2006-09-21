0f79ed8b7c6a56daa5f246b0c049968af0be63cd
Fred Drake <fdrake@acm.org>
2006-09-21 03:23:27 +0000

    isolate functional tests in a separate layer

--- src/zc/catalog/browser/ftests.py
+++ src/zc/catalog/browser/ftests.py
@@ -15,13 +15,48 @@
 
 $Id$
 """
-from zope.app.testing.functional import FunctionalDocFileSuite
-from zope.testing import doctest
+import doctest
+import os.path
+import unittest
+
+import transaction
+
+import zope.app.appsetup.bootstrap
+import zope.app.appsetup.interfaces
+import zope.app.intid
+import zope.app.intid.interfaces
+import zope.app.testing.functional
+
+
+here = os.path.dirname(os.path.realpath(__file__))
+
+ZcCatalogLayer = zope.app.testing.functional.ZCMLLayer(
+    os.path.join(here, "ftesting.zcml"), __name__, "ZcCatalogLayer")
+
+
+@zope.component.adapter(
+    zope.app.appsetup.interfaces.IDatabaseOpenedWithRootEvent)
+#
+def initializeIntIds(event):
+    db, connection, root, root_folder = (
+        zope.app.appsetup.bootstrap.getInformationFromEvent(event))
+    sm = root_folder.getSiteManager()
+    intids = zope.app.intid.IntIds()
+    sm["default"]["test-int-ids"] = intids
+    sm.registerUtility(
+        intids,
+        zope.app.intid.interfaces.IIntIds)
+    transaction.commit()
+    connection.close()
+
 
 def test_suite():
-    return FunctionalDocFileSuite('README.txt',
+    suite = zope.app.testing.functional.FunctionalDocFileSuite(
+        "README.txt",
         optionflags=doctest.ELLIPSIS|doctest.NORMALIZE_WHITESPACE)
+    suite.layer = ZcCatalogLayer
+    return suite
 
-if __name__ == '__main__':
-    import unittest
-    unittest.main(defaultTest='test_suite')
+
+if __name__ == "__main__":
+    unittest.main(defaultTest="test_suite")
