45d6c9e93d96ae3dcc12f56ba528d784feeaccbd
Stephan Richter <stephan.richter@gmail.com>
2007-11-03 18:34:33 +0000

    vb

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zc.catalog',
-      version = '1.2.0',
+      version = '1.3.0dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description="Extensions to the Zope 3 Catalog",
