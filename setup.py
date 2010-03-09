f263f0682d20a50fcafccf9f1550deca5649c97b
Patrick Strawderman <patrick@kilink.net>
2010-03-09 16:55:53 +0000

    Back to development: 1.4.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname('.'), *rnames)).read()
 
 setup(name='zc.catalog',
-      version = '1.4.3dev',
+      version = '1.4.4dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description="Extensions to the Zope 3 Catalog",
