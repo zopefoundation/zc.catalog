6b71cf78c21de35fc942090d9edbf943cba6e485
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2010-10-05 11:30:11 +0000

    Preparing release 1.4.5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname('.'), *rnames)).read()
 
 setup(name='zc.catalog',
-      version='1.4.5dev',
+      version='1.4.5',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description="Extensions to the Zope 3 Catalog",
