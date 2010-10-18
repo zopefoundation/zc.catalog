256df158cdc2ae37d4af77373bf6c4ed3ecd2a8d
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2010-10-18 08:52:55 +0000

    make the browser subpackage an optional dependency. as a result, projects that do not need the ZMI views to be registered, will not pull in zope.app.* packages anymore.

--- src/zc/catalog/browser/tests.py
+++ src/zc/catalog/browser/tests.py
@@ -18,45 +18,46 @@ $Id$
 import doctest
 import os.path
 import unittest
-
 import transaction
-
-import zope.app.appsetup.bootstrap
-import zope.app.appsetup.interfaces
-import zope.app.testing.functional
 import zope.intid
 import zope.intid.interfaces
 
+try:
+    # Only include the functional tests whenever the required dependencies
+    # are installed.
+    import zope.app.appsetup.bootstrap
+    import zope.app.appsetup.interfaces
+    import zope.app.testing.functional
 
-here = os.path.dirname(os.path.realpath(__file__))
+    here = os.path.dirname(os.path.realpath(__file__))
 
-ZcCatalogLayer = zope.app.testing.functional.ZCMLLayer(
-    os.path.join(here, "ftesting.zcml"), __name__, "ZcCatalogLayer")
+    ZcCatalogLayer = zope.app.testing.functional.ZCMLLayer(
+        os.path.join(here, "ftesting.zcml"), __name__, "ZcCatalogLayer")
 
+    @zope.component.adapter(
+        zope.app.appsetup.interfaces.IDatabaseOpenedWithRootEvent)
+    def initializeIntIds(event):
+        db, connection, root, root_folder = (
+            zope.app.appsetup.bootstrap.getInformationFromEvent(event))
+        sm = root_folder.getSiteManager()
+        intids = zope.intid.IntIds()
+        sm["default"]["test-int-ids"] = intids
+        sm.registerUtility(
+            intids,
+            zope.intid.interfaces.IIntIds)
+        transaction.commit()
+        connection.close()
 
-@zope.component.adapter(
-    zope.app.appsetup.interfaces.IDatabaseOpenedWithRootEvent)
-#
-def initializeIntIds(event):
-    db, connection, root, root_folder = (
-        zope.app.appsetup.bootstrap.getInformationFromEvent(event))
-    sm = root_folder.getSiteManager()
-    intids = zope.intid.IntIds()
-    sm["default"]["test-int-ids"] = intids
-    sm.registerUtility(
-        intids,
-        zope.intid.interfaces.IIntIds)
-    transaction.commit()
-    connection.close()
+    def test_suite():
+        suite = zope.app.testing.functional.FunctionalDocFileSuite(
+            "README.txt",
+            optionflags=doctest.ELLIPSIS|doctest.NORMALIZE_WHITESPACE)
+        suite.layer = ZcCatalogLayer
+        return suite
 
-
-def test_suite():
-    suite = zope.app.testing.functional.FunctionalDocFileSuite(
-        "README.txt",
-        optionflags=doctest.ELLIPSIS|doctest.NORMALIZE_WHITESPACE)
-    suite.layer = ZcCatalogLayer
-    return suite
-
+except (ImportError,), e:
+    def test_suite():
+        return unittest.TestSuite()
 
 if __name__ == "__main__":
     unittest.main(defaultTest="test_suite")
