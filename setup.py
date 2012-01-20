7ea0cd112acec810cfcb892ab8c289b2a4c42b4a
Gediminas Paulauskas <menesis@pov.lt>
2012-01-20 19:21:38 +0000

    Back to development: 1.6

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname('.'), *rnames)).read()
 
 setup(name='zc.catalog',
-      version='1.5.1',
+      version='1.6dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description="Extensions to the Zope 3 Catalog",
